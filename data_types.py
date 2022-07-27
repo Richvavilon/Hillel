fd27c0c1d4ccbbc1dbf64d61e0045baa4eab2e96
Sergiy <szakharchenko@gmail.com>
2022-07-27 15:40:04 +0300

    need to check3

---
+++ data_types.py
@@ -0,0 +1,30 @@
+ver_1 = 4
+print(ver_1, type(ver_1))
+
+ver_2 = 'test text'
+print(ver_2, type(ver_2))
+
+ver_3 = 54367
+print(ver_3, type(ver_3))
+
+a = 43
+print(a, type(a))
+
+b = 'abcd'
+print(b, type(b))
+
+c, d = 99, 'qwerty'
+print(c, type(c), ',', d, type(d))
+
+username = 'Sergio123'
+print(username, type(username))
+
+greeting = '''Hello world! That is so great to ...
+learn Python'''
+print(greeting, type(greeting))
+
+m_1 = 4
+m_2 = 2
+print(m_1 + m_2, m_1 - m_2, m_1 * m_2, m_1 / m_2, m_1 // m_2, m_1 % m_2, sep='\n')
+print(type(m_1), type(m_2), sep='\n')
+print(type(m_1 / m_2))
