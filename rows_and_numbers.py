cedb7b9785fd8b8a8148d0a48df4cce33d0d859c
Sergiy <szakharchenko@gmail.com>
2022-07-27 13:51:16 +0300

    need to check

---
+++ rows_and_numbers.py
@@ -0,0 +1,7 @@
+username = input('Please enter your name ')
+greetings = f'Hello, {username} nice to meet you!'
+print(greetings)
+
+some_number = float(input('Enter number '))
+print(some_number, '\n', int(some_number), '\n',
+      int(some_number) ** 4, '\n', int(some_number) * 0.5, '\n', int(some_number) % 2)
