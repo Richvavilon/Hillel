c5d596dc24438f5c8a49d0c74d1221ca875924f8
Sergiy <szakharchenko@gmail.com>
2022-07-27 15:24:04 +0300

    need to check2

--- rows_and_numbers.py
+++ rows_and_numbers.py
@@ -2,6 +2,5 @@ username = input('Please enter your name ')
 greetings = f'Hello, {username} nice to meet you!'
 print(greetings)
 
-some_number = float(input('Enter number '))
-print(some_number, '\n', int(some_number), '\n',
-      int(some_number) ** 4, '\n', int(some_number) * 0.5, '\n', int(some_number) % 2)
+some_num = float(input('Enter number '))
+print(some_num, int(some_num), int(some_num) ** 4, int(some_num) * 0.5, int(some_num) % 2, sep='\n')
