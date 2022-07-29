a485686bc94deb83eada88d6872baae3537e38f2
Sergiy <szakharchenko@gmail.com>
2022-07-29 15:14:35 +0300

    solved mistake. Please check

--- rows_and_numbers.py
+++ rows_and_numbers.py
@@ -3,4 +3,4 @@ greetings = f'Hello, {username} nice to meet you!'
 print(greetings)
 
 some_num = float(input('Enter number '))
-print(some_num, int(some_num), int(some_num) ** 4, int(some_num) * 0.5, int(some_num) % 2, sep='\n')
+print(some_num, int(some_num), int(some_num) ** 4, int(some_num) ** 0.5, int(some_num) % 2, sep='\n')
