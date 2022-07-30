5a80feef14b4129912ba60664afaab0a4312fdd9
Sergiy <szakharchenko@gmail.com>
2022-07-30 16:00:22 +0300

    Please check

---
+++ rows_and_numbers_advance.py
@@ -0,0 +1,7 @@
+username = input('Enter text ')
+greet = f'Hallo {username.capitalize()}, nice to meet you!'
+print(username.swapcase())
+print(username.strip())
+print(greet)
+print(len(username))
+print(username[:: -1])
